027f47a97e110a6524cf92f0e58d14cf984aa6e4
Christopher Porter <snooper90@gmail.com>
2016-04-03 11:06:00 -0500

    add open btn and logic to open option bar

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 var grid = document.getElementById("board");
 var option_overlay = document.querySelector('.option_overlay');
 close_option_btn = document.getElementById('close_option_btn');
+open_option_btn = document.getElementById('open_option_btn');
 
 close_option_btn.addEventListener('click', function(){
-  option_overlay.classList.add('hide')
+  option_overlay.classList.add('hide');
+});
+
+open_option_btn.addEventListener('click', function(){
+  option_overlay.classList.remove('hide');
 });
 
 grid.addEventListener("click", function(){
