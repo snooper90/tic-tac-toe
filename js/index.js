d7e656a938bbafdc013efd8a4a0b7a5dd4552113
Christopher Porter <snooper90@gmail.com>
2016-04-07 16:25:11 -0500

    created basic logic to change square to X or O

--- js/index.js
+++ js/index.js
@@ -14,3 +14,29 @@ open_option_btn.addEventListener('click', function(){
 grid.addEventListener("click", function(){
 
 });
+// code above line just for temp option menue usage
+
+
+//research the best way to set a turn variable
+
+var playersTurn = true;
+
+// selecting each td and adding an even listener
+$('tbody').click(function(e){
+  if(e.target.nodeName == 'TD'){
+    if(playersTurn){
+      $(e.target).text('X');
+      playersTurn = !playersTurn;
+      //check for winner
+    }else{
+      $(e.target).text('O');
+      playersTurn = !playersTurn;
+      //check for winner
+    };
+    console.log('click')
+  }
+});
+
+function checkForWinner(){
+
+}
