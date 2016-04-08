92ae7d576fbbdfaeaea0ca6a4ee91177572e5066
Christopher Porter <snooper90@gmail.com>
2016-04-08 12:09:06 -0500

    have the board working with data values and created a function to make the data values

--- js/index.js
+++ js/index.js
@@ -11,9 +11,6 @@ open_option_btn.addEventListener('click', function(){
   option_overlay.classList.remove('hide');
 });
 
-grid.addEventListener("click", function(){
-
-});
 // code above line just for temp option menue usage
 
 
@@ -22,28 +19,29 @@ grid.addEventListener("click", function(){
 var playersTurn = true;
 
 // selecting each td and adding an even listener
-$('tbody').click(function(e){
-  if(e.target.nodeName == 'TD'){
+$('td').click(function(e){
+  var $cell = $(e.currentTarget)
+
     if(playersTurn){
       $(e.target).text('X');
       playersTurn = !playersTurn;
       //test using data attribute instead of inner HTML
+      console.log(e.attributes)     ;
+      $cell.data('value', "o")
 
       //check for winner
     }else{
       $(e.target).text('O');
       playersTurn = !playersTurn;
-      //check for winner
+      $cell.data('value', "x")
     };
     console.log('click')
     checkForWinner();
-  }
 });
 
 function checkForWinner(){
 //collect bord data
-var arrBoard =$('td').text().split('');
-
+var arrBoard =getBoardData();
 //check the rows
 checkWinRow(arrBoard);
 checkWinCol(arrBoard);
@@ -54,21 +52,32 @@ checkWinCol(arrBoard);
 function checkWinRow(arrBoard){
   for(var i = 0; i < arrBoard.length ; i += 3){
     if(arrBoard[i] === arrBoard[i + 1] && arrBoard[i + 1] === arrBoard[i + 2]){
-      alert('working across');
-      return true;
+      if(arrBoard[i] !==   undefined){
+        alert('working across');
+        return true;
+      };
     };
   };
   return false
 };
 function checkWinCol(arrBoard){
-  for(var i = 0; i < 4 ; i++ ){
+  for(var i = 0; i < 3 ; i++ ){
     if(arrBoard[i] === arrBoard[i + 3] && arrBoard[i + 3] === arrBoard[i + 6]){
-      alert('working down')
-      return true
+      if(arrBoard[i] !== undefined){
+        alert('working down')
+        return true
+      };
     };
   };
   return false
 };
 
+function getBoardData(){
+  var arrBoard = [];
+  for(var i = 0 ; i < 9 ; i++){
+    arrBoard.push($($('td')[i]).data('value'));
+  };
+  return arrBoard;
+}
 
-// $('td').data('value') => 'x'
+// $('td#cell-0')[i].data('value') => 'x'
