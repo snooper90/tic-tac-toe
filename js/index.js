41679880d224a393d2e3e38d6cf8b7b1c5482761
Christopher Porter <snooper90@gmail.com>
2016-04-11 16:14:03 -0500

    cleaned up the code for readability

--- js/index.js
+++ js/index.js
@@ -1,21 +1,29 @@
+//set an obj that will keep track of player attributes and turn
 var players = {'player1':{'name': 'player1', 'symbol': 'X', 'wins': 0, 'img': '<img src="images/moveOne.png" alt="X">'},
                'player2':{'name': 'player2', 'symbol': 'O', 'wins': 0, 'img': '<img src="images/moveTwo.png" alt="X">'},
                'playersTurn':{'start': true, 'current': true }};
 
-// selecting each td and adding an even listener
+
 $().ready(function(){
+  //set visual for the score
   updateScore();
+  //set board values to null
   clearBoard();
+  // selecting each td and adding an even listener
   $('td').click(function(e){
-    var $cell = $(e.currentTarget);
-    var $cellValue = $(e.currentTarget).data('value');
+    var $cell = $(e.currentTarget);// The clicked cell location
+    var $cellValue = $(e.currentTarget).data('value');//the clicked square data value
+    //make sure square is empty. If empty continue
     if($cellValue === null){
+      //set square values based on the turn
       if(players.playersTurn.current){
         updateBoard($cell , players.player1.symbol, players.player1.img);
       }else{
         updateBoard($cell , players.player2.symbol, players.player2.img);
       };
+      //toggle turn
       players.playersTurn.current = !players.playersTurn.current;
+      //end of game functions below
       if(checkForDraw()){
         clearBoard();
         onWin();
@@ -42,7 +50,7 @@ function checkForWinner(){
     return checkWinDiag(arrBoard);
   };
 };
-
+// check each row
 function checkWinRow(arrBoard){
   for(var i = 0; i < 7 ; i += 3){
     if(arrBoard[i] === arrBoard[i + 1] && arrBoard[i + 1] === arrBoard[i + 2]){
@@ -53,7 +61,7 @@ function checkWinRow(arrBoard){
   };
   return false;
 };
-
+//check each column
 function checkWinCol(arrBoard){
   for(var i = 0; i < 3 ; i++ ){
     if(arrBoard[i] === arrBoard[i + 3] && arrBoard[i + 3] === arrBoard[i + 6]){
@@ -64,7 +72,7 @@ function checkWinCol(arrBoard){
   };
   return false;
 };
-
+//check the two diag ways to win
 function checkWinDiag(arrBoard){
   var arrBoard = getBoardData();
   if(arrBoard[0] === arrBoard[4] && arrBoard[4] === arrBoard[8]){
@@ -89,18 +97,18 @@ function getBoardData(){
 };
 
 function updateBoard($cell, player, img){
-  $cell.html(img);
-  $cell.data('value', player);
+  $cell.html(img);//update square image
+  $cell.data('value', player);//update value of square
 };
 
 function updateScore(winner){
-  //update counters
+  //increase counters if winner
   if(winner === players.player1.symbol){
     players.player1.wins += 1;
   }else if(winner === players.player2.symbol){
     players.player2.wins += 1;
   }
-  //update values
+  //update shown values
   $('#p1Wins').text(players.player1.wins);
   $('#p2Wins').text(players.player2.wins);
 };
@@ -122,15 +130,16 @@ function checkForDraw(){
 }
 
 function onWin(winner){
-  var $winMessage = $('#showOnWin');
+  var $winningMessage = $('#showOnWin');
 //set the winning message to show the winner
   if(winner === "X"){
-    $winMessage.text(players.player1.name + " wins!!! ");
+    $winningMessage.text(players.player1.name + " wins!!! ");
   }else if(winner === "O"){
-    $winMessage.text(players.player2.name + " wins!!! ");
+    $winningMessage.text(players.player2.name + " wins!!! ");
   }else{
-    $winMessage.text("Tie!");
+    $winningMessage.text("Tie!");
   };
+  //bring up overlay
   showWinner();
 };
 
